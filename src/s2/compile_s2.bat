cf1d21e6b6bba524848e528a0ce63f5cc97a536d
Brian Krauth <bkrauth@sfu.ca>
2020-03-16 15:13:20 -0700

    Compile s2 (closes #2)
    Signed-off-by: Brian Krauth <bkrauth@sfu.ca>

--- src/s2/compile_s2.bat
+++ src/s2/compile_s2.bat
@@ -10,5 +10,6 @@
 @rem ifort /O2 /Qipo /Qprec-div- /QaxHost /arch:IA32 -o s2.exe ../lib/bklib.f90 ../lib/bkmath.f90 s2glob.f90 loglik.f90 ../lib/dfpmin.f90 ../lib/simann.f90 smle2.f90 s2.f90
 @rem This is an experimental version
 @rem ifort /MT /Qopenmp-link:static /O2 /Qipo /Qprec-div- /Qparallel /QxHost -o s2a.exe ../lib/bklib.f90 ../lib/bkmath.f90 s2glob.f90 loglik.f90 ../lib/dfpmin.f90 ../lib/simann.f90 smle2.f90 s2.f90 
-ifort /O2 /Qipo /Qprec-div- /QaxHost /arch:IA32 -o s2b.exe ../lib/bklib.f90 ../lib/bkmath.f90 s2glob.f90 loglik.f90 ../lib/dfpmin.f90 ../lib/simann.f90 smle2.f90 s2.f90
- 
+ifort /O2 /Qipo /Qprec-div- /QaxHost /arch:IA32 -o s2.exe ../lib/bklib.f90 ../lib/bkmath.f90 s2glob.f90 loglik.f90 ../lib/dfpmin.f90 ../lib/simann.f90 smle2.f90 s2.f90
+
+del *.mod *.obj
